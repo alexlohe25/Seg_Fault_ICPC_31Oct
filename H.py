eb85ba5727bba39ff6c2c4cee3f828bff5bd7d60
DessTEC <71608725+DessTEC@users.noreply.github.com>
2020-10-31 14:16:57 -0600

    Add files via upload

---
+++ H.py
@@ -0,0 +1,23 @@
+test=int(input())
+
+for i in range (test):
+    total=[]
+    house=input()
+    total=input()
+    total=total.split()
+    print(total)
+    nonSum = 0
+    parSum = 0
+
+    for k in range(0,len(total),2):
+        parSum += int(total[k])
+    
+    for k in range(1,len(total),2):
+        nonSum += int(total[k])
+    
+    if(parSum > nonSum):
+        print(parSum)
+    elif(nonSum > parSum):
+        print(nonSum)
+    else:
+        print(parSum)
