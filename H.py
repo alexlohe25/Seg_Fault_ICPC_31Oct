a49d16a8e526f503dc8f09a302dfaf8ca8111c9c
DessTEC <71608725+DessTEC@users.noreply.github.com>
2020-10-31 14:55:41 -0600

    Add files via upload

--- H.py
+++ H.py
@@ -1,3 +1,23 @@
+def max_candy(total):
+    inclusivo=int(total[0])
+    exclusivo=0
+    
+    for i in range(1,len(total)):
+        if exclusivo>inclusivo:
+            new_exclusivo=exclusivo
+        else:
+            new_exclusivo=inclusivo
+        
+        inclusivo=exclusivo+int(total[i])
+        exclusivo= new_exclusivo
+    
+    if exclusivo>inclusivo:
+        return exclusivo
+    else:
+        return inclusivo
+
+
+
 test=int(input())
 
 for i in range (test):
@@ -5,18 +25,4 @@ for i in range (test):
     house=input()
     total=input()
     total=total.split()
-    nonSum = 0
-    parSum = 0
-
-    for k in range(0,len(total),2):
-        parSum += int(total[k])
-    
-    for k in range(1,len(total),2):
-        nonSum += int(total[k])
-    
-    if(parSum > nonSum):
-        print(parSum)
-    elif(nonSum > parSum):
-        print(nonSum)
-    else:
-        print(parSum)
+    print(max_candy(total))
